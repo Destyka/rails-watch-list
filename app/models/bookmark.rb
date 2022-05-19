83a8510f0822f318a48e67fcb34964c33f3151bf
Destyka <102505452+Destyka@users.noreply.github.com>
2022-05-19 13:23:42 +0200

    partie 1

---
+++ app/models/bookmark.rb
@@ -0,0 +1,6 @@
+class Bookmark < ApplicationRecord
+  belongs_to :movie
+  belongs_to :list
+  validates :movie_id, uniqueness: { scope: :list }, presence: true
+  validates :comment, length: { minimum: 6 }, presence: true
+end
